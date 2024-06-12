2b351a6bb0b1d1c094dbc4c8981fdd4159c1a269
Hojjat <hjt1377@gmail.com>
2024-06-12 10:39:51 -0700

    add athentication services to PublicApi project update gitIgnore

--- PublicApi/Program.cs
+++ PublicApi/Program.cs
@@ -1,11 +1,61 @@
+using System.Text;
+using ApplicationCore.Constants;
+using ApplicationCore.Interfaces;
 using Infrastructure.Context;
+using Infrastructure.Identity;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+///////////// Identity services ////////////////////////////////////
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(config =>
+{
+    config.SignIn.RequireConfirmedEmail = true;
+#warning  TODO:enable RequireUniqueEmail for production
+    //config.User.RequireUniqueEmail = true;
+    config.User.AllowedUserNameCharacters =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-";
+
+    config.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+})
+        .AddEntityFrameworkStores<AppIdentityDbContext>()
+        .AddDefaultTokenProviders();
+builder.Services.AddDbContext<AppIdentityDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityConnection")));
+builder.Services.AddTransient<IEmailSender>(sender => new EmailSender(EmailSendingConstants.EmailAddress, EmailSendingConstants.Password));
+builder.Services.AddTransient<ITokenClaimsService, IdentityTokenClaimService>();
+
+#warning  TODO:configure JWT settings as you need
+var key = Encoding.ASCII.GetBytes(AuthorizationConstants.JWT_SECRET_KEY);
+builder.Services.AddAuthentication(config =>
+{
+    config.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(config =>
+{
+    config.RequireHttpsMetadata = false;
+    config.SaveToken = true;
+    config.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+///////////////////////////////////////////////////////////////////////
+
+
 builder.Services.AddDbContext<CatalogContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("CatalogConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
